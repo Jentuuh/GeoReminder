569490748e639904e7d2fba2327d1e770a989ebb
jente-student <jente.vandersanden@student.uhasselt.be>
2020-02-21 15:16:06 +0100

    Added queries for latitude, longitude and remindermessage

--- app/src/main/java/com/example/georeminder/reminderDAO.java
+++ app/src/main/java/com/example/georeminder/reminderDAO.java
@@ -18,6 +18,30 @@ public interface reminderDAO {
     List<ReminderEntity> getAll();
 
     /**
+     * Gets the longitude of a certain reminder
+     * @param reminderID : ID of the reminder you're looking for
+     * @return : Returns the longitude of the reminder we were looking for.
+     */
+    @Query("SELECT longitude FROM reminder where ID LIKE :reminderID")
+    Double getLongitude(String reminderID);
+
+    /**
+     * Gets the latitude of a certain reminder
+     * @param reminderID : ID of the reminder you're looking for
+     * @return : Returns the latitude of the reminder we were looking for.
+     */
+    @Query("SELECT latitude FROM reminder where ID LIKE :reminderID")
+    Double getLatitude(String reminderID);
+
+    /**
+     * Gets the remindermessage of a certain reminder
+     * @param reminderID : ID of the reminder you're looking for
+     * @return : Returns the remindermessage of the reminder we were looking for.
+     */
+    @Query("SELECT message FROM reminder where ID LIKE :reminderID")
+    String getMessage(String reminderID);
+
+    /**
      * Inserts a new ReminderEntity into the database
      * @param reminder : reminder to be inserted
      */
